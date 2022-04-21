4372d77aa47dde6bd7976f416110eb325c2d14ea
Daniela-Flores <dfloresq1900@alumno.ipn.mx>
2022-04-21 04:25:19 -0500

    Logos navegadores

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -5,7 +5,10 @@ import featureimage from '../images/feature_1.svg';
 import featureimage1 from '../images/feature_2.svg';
 import featureimage2 from '../images/feature_3.svg';
 import duo from '../images/duo.png';
-import f_x_c from '../images/f_x_c.png';
+import edge from '../images/edge_logo.png';
+import opera from '../images/opera_logo.png';
+import chromium from '../images/chromium_logo.png';
+import brave from '../images/brave_logo.png';
 import { Col, Container, Form, Button, Card } from "react-bootstrap";
 
 function Feature() {
@@ -20,30 +23,20 @@ function Feature() {
         <div id='features'>
             <h2>Extensión en tu navegador favorito</h2>
             <div className='a-container'>
-            <Image src={f_x_c} style={{width: 350, height: 350}} alt='...' />
+            <Image className='navegadores' src={edge} style={{width: 250, height: 250}} alt='...' />
+            <Image className='navegadores' src={opera} style={{width: 250, height: 250}} alt='...' />
+            <Image className='navegadores' src={chromium} style={{width: 250, height: 250}} alt='...' />
+            <Image className='navegadores' src={brave} style={{width: 250, height: 250}} alt='...' />
             </div>
         </div>
-        <div id='features2'>
-            
-            <h2>¿Qué es Whalefare?</h2>
-            <p>Lo que hacemos y por qué lo hacemos</p>
-            <div className='parrafoini' >
-              <p >En la actualidad la vida de la mayoría de personas converge con los servicios digitales
-                  ofrecidos por un gran número de compañías tecnológicas. Estos servicios son personalizables,
-                  pues permiten al usuario expresar su personalidad en estos servicios para establecer una conexión y destacar su 
-                  individualidad, razón por la cual existen las cuentas. Las contraseñas necesitan garantizar la seguridad de sus respectivas cuentas.
-
-              </p>
-              
-            </div>
-        </div>
+        <div id="features2"><h2>Qué es Whalefare</h2><p>Lo que hacemos y por qué lo hacemos</p><div class="parrafoini"><p>En la actualidad, la vida de la población converge con los servicios digitales existentes en un amplio número de rubros: desde la escritura de un dato hasta la realización de operaciones financieras. Sin embargo, conforme estos elementos aumentan, el cuidado y la atención prestados disminuyen.</p><p>Whalefare es un gestor de datos que permite guardar y administrar notas, tarjetas de crédito y contraseñas, así como generar estas últimas, con la finalidad de proveer un ambiente amigable, con facilidad de uso y rutas cifradas.</p></div></div>
         <div id='features3'>
             
             <h2>Whalefare prioriza tu seguridad</h2>
             <p>Descubre cómo protegemos tus contraseñas</p>
 
             <div className='iconosf3'>
-                <FeatureBox image={featureimage} title='Gráficos' name='A través de un sistema comprensible, sabrás cuán seguras son tus contraseñas.' />
+                <FeatureBox image={featureimage} title='Accesibilidad' name='Mediante el almacenamiento en la nube, podrás acceder a tus datos desde cualquier dispositivo.' />
                 <FeatureBox image={featureimage1} title='Cifrado' name='Gracias al uso de cifrado, tus datos se encuentran seguros en la aplicación.' />
                 <FeatureBox image={featureimage2} title='Facilidad' name='Con una interfaz fácil de usar, podrás gestionar tus datos sensibles.' />
             </div>
