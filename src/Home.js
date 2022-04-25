458dd31bcaa2d76c1cba09e04fa02881e3bcb1bf
CarapiaOwO <jcarapiaa1900@alumno.ipn.mx>
2022-04-24 22:22:26 -0500

    Color del header contraseña 0.2

--- src/Home.js
+++ src/Home.js
@@ -300,7 +300,7 @@ class Home extends Component {
                                                                     <div className="col">
                                                                         <div className="card">
                                                                             <div className='center-text'>
-                                                                                <h4 className="card-header">{pass.title_c}</h4>
+                                                                                <h4 className="card-header  bg-primary">{pass.title_c}</h4>
                                                                             </div>
                                                                             {this.state.authorized !== 1 ?
                                                                                 /*CONTENEDOR PREVIO A AUTENTICACIÓN */
@@ -321,7 +321,7 @@ class Home extends Component {
                                                                                         <input
                                                                                             name="Title"
                                                                                             id={"title" + pass.id_c}
-                                                                                            className="form-control bg-primary"
+                                                                                            className="form-control"
                                                                                             disabled={true}
                                                                                             value={pass.title_c}
                                                                                             
